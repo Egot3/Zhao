57b8f3d1b935e2815b4c742dfa0f054f8755ef65
Egot3 <noobicklp@gmail.com>
2026-02-21 18:36:15 +0300

    unnamed

--- queues/testQueue.go
+++ queues/testQueue.go
@@ -31,3 +31,8 @@ func NewQueue(ch *amqp.Channel, q QueueStruct) (*amqp.Queue, error) { //FACTORY 
 
 	return &queue, nil
 }
+
+func DeleteQueue(ch *amqp.Channel, q QueueStruct) error {
+	_, err := ch.QueuePurge(q.Name, q.NoWait)
+	return err
+}
