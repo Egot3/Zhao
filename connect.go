1070c006c52bd1a19c6513babc9e0a37d691640d
Egot3 <noobicklp@gmail.com>
2026-02-22 22:58:43 +0300

    WHAT WAS I THINKING

--- connect.go
+++ connect.go
@@ -3,7 +3,6 @@ package diacon
 import (
 	"fmt"
 
-	"github.com/Egot3/Zhao/bindings"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -13,28 +12,11 @@ type RabbitMQConfiguration struct {
 	Port string //Port(string) w/o :
 }
 
-type Connection struct {
-	amqp.Connection
-	channel bindings.PubSubChannel
-}
-
 // Get a connection for given configuration(will be passed to pubs and subs)
-func Connect(cfg RabbitMQConfiguration) (*Connection, error) {
+func Connect(cfg RabbitMQConfiguration) (*amqp.Connection, error) {
 	conn, err := amqp.Dial(fmt.Sprintf("%v:%v/", cfg.URL, cfg.Port))
 	if err != nil {
 		return nil, err
 	}
-	ch, err := conn.Channel()
-	if err != nil {
-		return nil, err
-	}
-
-	return &Connection{
-		*conn,
-		bindings.PubSubChannel{Ch: ch},
-	}, nil
-}
-
-func (c *Connection) Channel() *bindings.PubSubChannel {
-	return &c.channel
+	return conn, nil
 }
